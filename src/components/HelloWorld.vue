5858d28746df63495124295d4a3413676b9956d9
panzerstadt <tliqun@gmail.com>
2019-06-13 15:12:14 +0900

    todolist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,57 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
-
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "Todo",
+  data() {
+    return {
+      todos: [],
+      input: ""
+    };
+  },
+  methods: {
+    add() {
+      this.todos.push(this.input);
+      this.input = "";
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<template>
+  <div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="well">
+          <h2>Create a New Todo</h2>
+          <form v-on:submit="(e) => {e.preventDefault(); add();}">
+            <div class="form-group">
+              <label for="todoitem">Todo Item</label>
+              <input
+                class="form-control"
+                type="text"
+                v-model="input"
+                placeholder="Todo Item"
+                name="todo-input"
+                id="todoitem"
+              >
+              <br>
+              <button type="button" v-on:click="add()" class="btn btn-default">Create</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div class="col-md-12">
+        <h2>List</h2>
+        <ul class="list-group">
+          <li
+            v-for="(todo, index) in todos"
+            :key="index"
+            class="list-group-item"
+          >{{index + 1}} : {{todo}}</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row"></div>
+  </div>
+</template>
